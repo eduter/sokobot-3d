d35a8b804b65dd57e5e959deb91dff79b21d6ba9
eduter <eduter@fareoffice.com>
2020-03-20 16:57:54 +0100

    Created a dummy main menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MainMenu from './components/MainMenu';
+import { Box, Main } from 'grommet';
+import styled from 'styled-components';
+
+function Header() {
+  const H1 = styled.h1`
+    text-align: center;
+  `;
+
+  return (
+    <header>
+      <H1>Sokobot 3D</H1>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main align="center">
+      <Box width="medium">
+        <Header/>
+        <MainMenu/>
+      </Box>
+    </Main>
   );
 }
 
