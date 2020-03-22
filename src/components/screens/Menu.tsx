a63942a91c2a9573e5dadc79069ea04d5dc2b8e7
eduter <eduter@fareoffice.com>
2020-03-22 12:23:48 +0100

    Refactored menu screen to use connect

--- src/components/screens/Menu.tsx
+++ src/components/screens/Menu.tsx
@@ -1,30 +1,40 @@
+import { push } from 'connected-react-router';
 import { Box, Button } from 'grommet';
 import { Play, SettingsOption } from 'grommet-icons';
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 
 
 const H1 = styled.h1`
   text-align: center;
 `;
 
-function Menu() {
+interface MenuProps {
+  startGame: () => void,
+  openSettings: () => void
+}
+
+function Menu({ startGame, openSettings }: MenuProps) {
   return (
     <Box width="medium">
       <header>
         <H1>Sokobot 3D</H1>
       </header>
       <Box width="medium" gap="small" align="stretch">
-        <Link to="/select-level">
-          <Button icon={<Play/>} label="Start Game"/>
-        </Link>
-        <Link to="/settings">
-          <Button icon={<SettingsOption/>} label="Settings"/>
-        </Link>
+        <Button icon={<Play/>} label="Start Game" onClick={startGame}/>
+        <Button icon={<SettingsOption/>} label="Settings" onClick={openSettings}/>
       </Box>
     </Box>
   );
 }
 
-export default Menu;
+function mapDispatchToProps(dispatch: Dispatch) {
+  return {
+    startGame: () => dispatch(push('/select-level')),
+    openSettings: () => dispatch(push('/settings'))
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Menu);
