fc1af7169fadf5c178bb585dd254928eae7d69e0
eduter <eduter@fareoffice.com>
2020-04-04 00:34:56 +0200

    Handling level cleared

--- src/components/screens/Level/Level.tsx
+++ src/components/screens/Level/Level.tsx
@@ -1,39 +1,47 @@
 import { goBack } from 'connected-react-router';
 import { Button } from 'grommet';
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect, ConnectedProps } from 'react-redux';
 import { Redirect, RouteComponentProps } from 'react-router';
 import { Dispatch } from 'redux';
 import levels from '../../../data/levels.json';
-import { levelsActions } from '../../../state/ducks/levels';
-import BackButton from '../../BackButton';
+import { levelsActions, levelsSelectors } from '../../../state/ducks/levels';
 import Screen from '../../Screen';
 import MyCanvas from '../../webgl/MyCanvas';
+import LevelClearedDialog from './LevelClearedDialog';
+import { State } from '../../../state/types';
 
 
 interface MatchParams {
   level: string;
 }
 
-interface LevelProps extends RouteComponentProps<MatchParams> {
-  finishLevel(level: number): void;
+interface LevelProps extends RouteComponentProps<MatchParams>, ConnectedProps<typeof connector> {
 }
 
-function Level({ match, finishLevel }: LevelProps) {
+function Level({ match, isUnlocked, finishLevel }: LevelProps) {
   const level = +match.params.level;
   const levelData = levels[level];
+  const nextLevel = levels[level + 1] ? level + 1 : undefined;
 
-  if (!levelData) {
+  if (!levelData || !isUnlocked(level)) {
     return <Redirect to="/select-level"/>;
   }
-
   return (
     <Screen title={levelData.name}>
       <Button label="Finish level" onClick={() => finishLevel(level)} />
       <MyCanvas/>
-      <BackButton/>
+      <LevelClearedDialog nextLevel={nextLevel}/>
     </Screen>
   );
+}
+
+function mapStateToProps(state: State) {
+  return {
+    isUnlocked(level: number) {
+      return levelsSelectors.isUnlocked(state.levels, level);
+    }
+  };
 }
 
 function mapDispatchToProps(dispatch: Dispatch) {
@@ -45,5 +53,7 @@ function mapDispatchToProps(dispatch: Dispatch) {
   };
 }
 
+const connector = connect(mapStateToProps, mapDispatchToProps);
 
-export default connect(null, mapDispatchToProps)(Level);
+export default connector(Level);
+export { Level };
---
+++ src/components/screens/Level/LevelClearedDialog.tsx
@@ -0,0 +1,70 @@
+import { push } from 'connected-react-router';
+import { Box, Button } from 'grommet';
+import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { Dispatch } from 'redux';
+import styled from 'styled-components';
+import levels from '../../../data/levels.json';
+import isLevelCleared from '../../../mechanics/isLevelCleared';
+import { LevelMap } from '../../../mechanics/types';
+import { gameActions } from '../../../state/ducks/game';
+import { levelsActions } from '../../../state/ducks/levels';
+import { State } from '../../../state/types';
+
+
+interface LevelClearedDialogProps extends ConnectedProps<typeof connector> {
+  nextLevel: number | undefined;
+}
+
+const DialogWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1000;
+  background-color: rgba(200, 200, 200, 0.5);
+  border-radius: 1em;
+  h1 {
+    text-align: center;
+  }
+`;
+
+function LevelClearedDialog({ isLevelCleared, nextLevel, goToLevel, goToLevelSelection }: LevelClearedDialogProps) {
+  if (isLevelCleared()) {
+    return (
+      <DialogWrapper>
+        <Box width="medium" gap="small" align="stretch" pad="small">
+          <h1>{nextLevel ? 'Level Cleared' : 'Game Cleared'}</h1>
+          {nextLevel !== undefined && <Button label="Go to next level" onClick={() => goToLevel(nextLevel)}/>}
+          {/*<Button label="Back to level selection" onClick={goToLevelSelection}/>*/}
+        </Box>
+      </DialogWrapper>
+    );
+  }
+  return null;
+}
+
+function mapStateToProps(state: State) {
+  return {
+    isLevelCleared: () => !!state.game && isLevelCleared(state.game)
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+  return {
+    goToLevel(level: number) {
+      dispatch(levelsActions.finishLevel(level - 1)); // TODO: refactor this mess
+      dispatch(push(`/level/${level}`));
+      dispatch(gameActions.startGame(levels[level].map as unknown as LevelMap));
+    },
+    goToLevelSelection() {
+      dispatch(push('/select-level'));
+    }
+  };
+}
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+
+export default connector(LevelClearedDialog);
+export { LevelClearedDialog };
