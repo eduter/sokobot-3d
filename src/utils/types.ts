cc68a0b81e4a28bec5b1b264032ddd25702fe9ee
eduter <eduter@fareoffice.com>
2020-04-29 11:50:43 +0200

    Minor refactoring

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -10,13 +10,9 @@ export type Maybe<T> = T | undefined;
  * Use this function in the case default of a switch that should exhaustively check all possible values.
  * It has no effect in runtime, but TS can catch in compile time if one possible value was forgotten.
  */
-function assertNever(value: never) {
+export function assertNever(value: never) {
   return value;
 }
-
-export {
-  assertNever
-};
 
 /**
  * Type for Redux actions.
