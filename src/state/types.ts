a91a0b6022d11ee7e949b38e5fd0e7b4e689b5b5
eduter <eduter@fareoffice.com>
2020-04-29 15:34:43 +0200

    Fixed test coverage report

--- src/state/types.ts
+++ src/state/types.ts
@@ -2,11 +2,10 @@ import { RouterState } from 'connected-react-router';
 import { History } from 'history';
 import { State as GameState } from './ducks/game/types';
 import { State as LevelsState } from './ducks/levels/types';
-import PoorMansUnknown = History.PoorMansUnknown;
 
 
 export type State = {
   levels: LevelsState;
   game: GameState;
-  router: RouterState<PoorMansUnknown>;
+  router: RouterState<History.PoorMansUnknown>;
 }
