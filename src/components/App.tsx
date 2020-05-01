70cdf6cb345772024252c0c9a98f6ed530504abe
eduter <eduter@fareoffice.com>
2020-05-02 00:31:22 +0200

    Lazily loading routes other than /

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,15 @@
 import { ConnectedRouter } from 'connected-react-router';
 import { Grommet, Main, ThemeType } from 'grommet';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router';
 import theme from '../theme';
 import { configureStore } from '../state';
-import Level from './screens/Level';
-import LevelSelection from './screens/LevelSelection';
 import Menu from './screens/Menu';
-import Settings from './screens/Settings';
+
+const Level = lazy(() => import('./screens/Level'));
+const LevelSelection = lazy(() => import('./screens/LevelSelection'));
+const Settings = lazy(() => import('./screens/Settings'));
 
 
 const { store, history } = configureStore();
@@ -19,13 +20,15 @@ function App() {
       <ConnectedRouter history={history}>
         <Grommet full theme={theme as ThemeType}>
           <Main align="center">
-            <Switch>
-              <Route exact path="/" component={Menu}/>
-              <Route exact path="/settings" component={Settings}/>
-              <Route exact path="/select-level" component={LevelSelection}/>
-              <Route exact path="/level/:level" component={Level}/>
-              <Route render={() => <Redirect to="/"/>}/>
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <Route exact path="/" component={Menu}/>
+                <Route exact path="/settings" component={Settings}/>
+                <Route exact path="/select-level" component={LevelSelection}/>
+                <Route exact path="/level/:level" component={Level}/>
+                <Route render={() => <Redirect to="/"/>}/>
+              </Switch>
+            </Suspense>
           </Main>
         </Grommet>
       </ConnectedRouter>
